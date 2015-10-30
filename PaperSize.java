20878042ea70b8679d764e36062f5444fbb1e70f
ahla_abir arbid <ahla_abir arbid>
2015-10-30 22:35:58 +0000

    end of checking input validity

--- PaperSize.java
+++ PaperSize.java
@@ -18,21 +18,43 @@ public class PaperSize
 		
 		System.out.println("Enter the the paper size to calculate dimensions");
 		String str = System.console().readLine();
-		//System.out.println("initial dimesnions are: x"+paper.x+" and "+paper.y);
-		
-		//check the input validity
-		String str1 = str.substring(0, 1);
-		//System.out.println(str1);
-		if (str1.equals("A")){
-			System.out.println("valid");
-		}
-			
-		
-		
-		
-		
-		
+		System.out.println(validity(str));
+				
 	}
+	//check the input validity
+		public Boolean validity(String str){
+		 Boolean valid = false;
+		 String str1 = str.substring(0, 1);
+		  if (str1.equals("A"))
+		  {
+			String str2 = str.substring(1);
+			int l = str2.length();
+			int i;
+			int j = 0;
+			String str3;
+			 if(l == 1){
+				for(i=0; i<9; i++){
+				 if (str2.substring(0, 1).equals("+i+"))
+					
+					valid = true;		 
+				}
+			}			
+			else {
+			 int t = 0;
+			  for(i=0; i<l; i++){				
+				j++;
+				str3 = str2.substring(i, j);
+				 if(str3.equals("0"))
+				 t++;				  
+			   if (t == l){
+				valid = true;
+				}
+			 }
+			}
+		  
+		}
+		return valid;	
+		}
 
 	class Paper {
  double x;

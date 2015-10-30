7e45f79857e927a18aa1095837bd9520c3361941
ahla_abir arbid <ahla_abir arbid>
2015-10-30 20:43:01 +0000

    working on the validity of the input

--- PaperSize.java
+++ PaperSize.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class PaperSize 
 { 
 	public static void main(String[] args) 
@@ -9,12 +10,25 @@ public class PaperSize
 
 	private void launch() {
 		
-		Paper paper = new Paper();
 		double  x0 = 841;
 		double y0 = 1189;
+		Paper paper = new Paper();
+		paper.x = x0;
+		paper.y = y0;
 		
 		System.out.println("Enter the the paper size to calculate dimensions");
 		String str = System.console().readLine();
+		//System.out.println("initial dimesnions are: x"+paper.x+" and "+paper.y);
+		
+		//check the input validity
+		String str1 = str.substring(0, 1);
+		//System.out.println(str1);
+		if (str1.equals("A")){
+			System.out.println("valid");
+		}
+			
+		
+		
 		
 		
 		

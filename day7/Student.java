b446221080ec36ccca6a38c85ee30a9889ffc126
ahla_abir arbid <ahla_abir arbid>
2015-11-18 17:34:48 +0000

    adding constructors

--- day7/Student.java
+++ day7/Student.java
@@ -3,4 +3,37 @@ public class Student{
 	private int year;
 	private Student next;
 	private Student prev;
+
+		public String getName(){
+			return name;
+		}
+		
+		public void setName(String name){
+			this.name = name;
+		}
+		
+		public int getYear(){
+			return year;
+		}
+		
+		public void setYear(int year){
+			this.year = year;
+		}
+		
+		public Student getNext(){
+			return next;
+		}
+		public void setNext(Student nextStudent){
+			this.next = nextStudent;
+		}
+		
+		public Student getPrev(){
+			return prev;
+		}
+		
+		public void setPrev(Student prevStudent){
+			this.prev = prevStudent;
+		}
+		
+		
 }

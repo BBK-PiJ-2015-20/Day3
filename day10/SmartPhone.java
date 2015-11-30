9d98c08a3610867dea8687854564da7478a5340e
ahla_abir arbid <ahla_abir arbid>
2015-11-30 20:54:57 +0000

    exercise10-2 in class

--- day10/SmartPhone.java
+++ day10/SmartPhone.java
@@ -1,7 +1,14 @@
 public class SmartPhone extends MobilePhone{
 	void browseWeb(String s3){
 	}
-	int findPosition(){
+	
+	public void call(String number){
+		if(number.substring(0,2).equals("00") ){
+			System.out.println("calling " + number + " through internet to save money");
+		}
+		else {
+			super.call(number);
+		}
 	}
 	
 }
